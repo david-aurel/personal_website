e64310a1c033acdfdfda66533fd97dc0cf5c217f
David Durlan <david.a.durlan@gmail.com>
2020-09-25 22:54:57 +0200

    big redesign 😍

---
+++ main.js
@@ -0,0 +1,6 @@
+window.onload = (() => {
+  const projectsLink = document.querySelector(".main-link a");
+  projectsLink.addEventListener("click", () => {
+    alert("I'm working on it 😐");
+  });
+})();
