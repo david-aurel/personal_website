d421f1bf357f9777e88944e414a35a2f5e835083
David Durlan <david.a.durlan@gmail.com>
2020-09-26 16:52:30 +0200

    delete js

--- main.js
+++
@@ -1,6 +0,0 @@
-window.onload = (() => {
-  const projectsLink = document.querySelector(".main-link");
-  projectsLink.addEventListener("click", () => {
-    alert("I'm working on it 😐");
-  });
-})();
