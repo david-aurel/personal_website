d578d1a26df303faab5bd3e941653b7ec6481f22
David Durlan <david.a.durlan@gmail.com>
2021-06-10 02:03:52 +0200

    🙆‍♀️ add copy-email-to-clipboard button

---
+++ copyEmail.js
@@ -0,0 +1,17 @@
+const email = document.querySelector("#email").text;
+const button = document.querySelector(".copy-button");
+
+button.addEventListener("click", function () {
+  navigator.clipboard
+    .writeText(email)
+    .then(function () {
+      if (button.classList.value.includes("success")) return;
+      button.setAttribute("aria-label", "email address copied to clipboard");
+      button.setAttribute("role", "alert");
+      button.innerHTML += "copied";
+      button.classList.add("success");
+    })
+    .catch(function (e) {
+      console.error(e);
+    });
+});
