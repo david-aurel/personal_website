642ef77f7e50d70fe49e3e25d842303af9d042d2
David Durlan <53895961+david-aurel@users.noreply.github.com>
2022-07-09 11:54:14 +0200

    feat: animate lizard 🦎

---
+++ lizard/index.js
@@ -0,0 +1,70 @@
+/* DOM references */
+var lizardImg = document.getElementById("lizard-head");
+var lizardImgRadius = lizardImg.offsetWidth / 2;
+
+/* updates the DOM */
+function update(updateParams) {
+  var cursorLocation = updateParams.cursorLocation;
+  var lizardLocation = updateParams.lizardLocation;
+
+  var closestPointFromLizardCircumferenceToCursor =
+    getCircumferenceIntersection({
+      p1: cursorLocation,
+      p2: lizardLocation,
+      r: lizardImgRadius
+    });
+  var angleToCursor = getAngleFromPoints({
+    p2: cursorLocation,
+    p1: closestPointFromLizardCircumferenceToCursor
+  });
+  var isFacingLeft = angleToCursor > 180;
+  var cantTurnNeckAnyMore = angleToCursor > 350 || angleToCursor < 20;
+  if (cantTurnNeckAnyMore) return;
+  lizardImg.style.transform =
+    "rotate(" + angleToCursor + "deg) " + (isFacingLeft ? "scaleX(-1)" : "");
+}
+
+/* gets closest point to cursor on lizard head circumference */
+function getCircumferenceIntersection(getCircumferenceIntersectionParamters) {
+  var p1 = getCircumferenceIntersectionParamters.p1;
+  var p2 = getCircumferenceIntersectionParamters.p2;
+  var r = getCircumferenceIntersectionParamters.r;
+
+  var dx = p2.x - p1.x;
+  var dy = p2.y - p1.y;
+  var mag = Math.hypot(dx, dy);
+  return {
+    x: p2.x - (r * dx) / mag,
+    y: p2.y - (r * dy) / mag
+  };
+}
+
+/* calculates the angle between lizard head and cursor */
+function getAngleFromPoints(getAngleFromPointsParameters) {
+  var p1 = getAngleFromPointsParameters.p1;
+  var p2 = getAngleFromPointsParameters.p2;
+
+  var atan = Math.atan2(p2.y - p1.y, p2.x - p1.x) * (180 / Math.PI);
+  var rotated = atan + 90;
+  var unsigned = rotated < 0 ? rotated + 360 : rotated;
+  return unsigned;
+}
+
+var fps = 12; // how many times to fire the event per second
+var wait = false;
+window.addEventListener("mousemove", function (e) {
+  if (!wait) {
+    /* throttled */
+    var cursorLocation = { x: e.clientX, y: e.clientY };
+    var lizardLocation = {
+      x: lizardImg.getBoundingClientRect().left + lizardImg.offsetWidth / 4,
+      y: lizardImg.getBoundingClientRect().top + lizardImg.offsetHeight
+    };
+    update({ cursorLocation: cursorLocation, lizardLocation: lizardLocation });
+    /*---------------*/
+    wait = true;
+    setTimeout(function () {
+      wait = false;
+    }, 1000 / fps);
+  }
+});
