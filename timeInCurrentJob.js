50629a17c100109f55b5c3dc7afab0a569726d86
David Durlan <53895961+david-aurel@users.noreply.github.com>
2022-11-11 12:30:32 +0100

    refactor: timeInCurrentJob variable naming 👩🏻‍🔧

--- timeInCurrentJob.js
+++ timeInCurrentJob.js
@@ -4,56 +4,58 @@ var currentJobStart = new Date("2021-02-01");
 
 var timeElapsedInMilliseconds = new Date() - currentJobStart;
 
-var timeElapsed = convertMilliseconds(timeElapsedInMilliseconds);
+var timeElapsed = convertMillisecondsToYearsAndMonths(
+  timeElapsedInMilliseconds
+);
 
-var monthTextFormatted = "";
-switch (timeElapsed.month) {
+var monthsTextFormatted = "";
+switch (timeElapsed.months) {
   case 0: {
     break;
   }
   case 1: {
-    monthTextFormatted = timeElapsed.month + " mo";
+    monthsTextFormatted = timeElapsed.months + " mo";
     break;
   }
   default: {
-    monthTextFormatted = timeElapsed.month + " mos";
+    monthsTextFormatted = timeElapsed.months + " mos";
     break;
   }
 }
 
-var yearTextFormatted = "";
-switch (timeElapsed.year) {
+var yearsTextFormatted = "";
+switch (timeElapsed.years) {
   case 0: {
     break;
   }
   case 1: {
-    yearTextFormatted = timeElapsed.year + " yr";
+    yearsTextFormatted = timeElapsed.years + " yr";
     break;
   }
   default: {
-    yearTextFormatted = timeElapsed.year + " yrs";
+    yearsTextFormatted = timeElapsed.years + " yrs";
     break;
   }
 }
 
 textNode.innerText +=
   " · " +
-  yearTextFormatted +
-  (monthTextFormatted ? " " + monthTextFormatted : "");
+  yearsTextFormatted +
+  (monthsTextFormatted ? " " + monthsTextFormatted : "");
 
-function convertMilliseconds(time) {
-  var month = Math.floor(
-    time / 2592000000 // 2592000000 = 1000 (seconds) * 60 (minutes) * 60 (hours) * 24 (days) * 30 (months)
+function convertMillisecondsToYearsAndMonths(time) {
+  var months = Math.floor(
+    time / 2592000000 // 2592000000 = 1000 (=>seconds) * 60 (=>minutes) * 60 (=>hours) * 24 (=>days) * 30 (=>months)
   );
 
-  var year = Math.floor(month / 12);
+  var years = Math.floor(months / 12);
 
-  month = (month % 12) + 1; // +1 for the current month
+  months = (months % 12) + 1; // +1 for the current month
 
-  if (month === 12) {
-    year += 1;
-    month = 0;
+  if (months === 12) {
+    years += 1;
+    months = 0;
   }
 
-  return { year, month };
+  return { years, months };
 }
